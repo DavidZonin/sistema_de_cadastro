678f234a45b95153e30702812068a4fbb8016147
DavidZonin <davidzonin12@gmail.com>
2023-06-23 23:03:44 -0300

    voltando sem o Paulo

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
-console.log("Rodando o sistema de gerenciamento de jogos");
-console.log("aplicação está se conectando aobanco de dados");
-console.log("enviando para o GitHub")
+console.log("Rodando o sistema de gerenciamento de jogos")
